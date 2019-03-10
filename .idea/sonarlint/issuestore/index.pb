20313a462881d356d7f07419b1b196915c42dfa1
vidavenel <vincent_mil@hotmail.fr>
2019-03-10 17:15:35 +0100

    commit init

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,19 @@
+
+I
+src/BoServiceProvider.php,4\9\493feb572fbba5f4c39b86483b779d26b65d64fc
+S
+#src/Responses/DashboardResponse.php,3\5\35f9a028fa5f6c9431f071050a3e63355a7fdfd9
+=
+composer.json,3\9\3957b91a0d8450cea0115cfd5af69eac90d75897
+W
+'src/Responses/CommandeIndexResponse.php,9\8\982740bdee65fde6e25c75a70a8532a90d7e79c4
+P
+ src/Interfaces/BaseInterface.php,3\d\3d5b7aeb956b36c1e8823ad34e7668c3b1c136d8
+R
+"src/Interfaces/ClientInterface.php,8\2\82c05820263bce907ad9986bc499aed69c397460
+>
+webpack.mix.js,7\3\73a5f9e927f70e0292701d67f61dbff4fb17febc
+S
+#resources/views/dashboard.blade.php,c\b\cb9b83605560606e98f1366cfe970597db7f6f83
+U
+%resources/views/layouts/app.blade.php,4\e\4e52e2a63befd1dc69600001e78b80ebece1ac9b
---
+++ tests/cache/f839d9fd2c51d34e6ebeed74df43c81b7402b7a9.php
@@ -0,0 +1,32 @@
+<?php /* D:\Projets\topcartegrise\tcgv2_bo\resources\views/dashboard.blade.php */ ?>
+<?php $__env->startSection('content'); ?>
+    <!-- Main content -->
+    <section class="content">
+
+        <!-- Default box -->
+        <div class="box">
+            <div class="box-header with-border">
+                <h3 class="box-title">Title</h3>
+
+                <div class="box-tools pull-right">
+                    <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip" title="Collapse">
+                        <i class="fa fa-minus"></i></button>
+                    <button type="button" class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Remove">
+                        <i class="fa fa-times"></i></button>
+                </div>
+            </div>
+            <div class="box-body">
+                Start creating your amazing application!
+            </div>
+            <!-- /.box-body -->
+            <div class="box-footer">
+                Footer
+            </div>
+            <!-- /.box-footer-->
+        </div>
+        <!-- /.box -->
+
+    </section>
+    <!-- /.content -->
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('bo::layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
