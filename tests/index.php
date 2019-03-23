c621d776042cb96e29bc8480df0f3195824c3f74
Vincent DAVENEL <vincent@aigle-developpement.com>
2019-03-23 23:07:06 +0100

    view ns

--- tests/index.php
+++ tests/index.php
@@ -7,21 +7,27 @@
  */
 
 use Illuminate\Http\Request;
-use Mejta\StandaloneBlade;
+use Jenssegers\Blade\Blade;
 
 require __DIR__ . '/../vendor/autoload.php';
 
 function view($view, $data)
 {
-    $viewDirs = [
+/*    $viewDirs = [
         __DIR__ . '/../resources/views',
     ];
     $cacheDir = __DIR__ . '/cache';
     $engine = new StandaloneBlade($viewDirs, $cacheDir);
     // Render template
-    echo $engine->render($view, $data);
+    echo $engine->render($view, $data);*/
+    $viewDirs = [
+        __DIR__ . '/../resources/views',
+    ];
+    $cacheDir = __DIR__ . '/cache';
+    $blade = new Blade($viewDirs, $cacheDir);
+    $blade->addNamespace('bo', $viewDirs);
+    echo $blade->make($view, $data);
 }
-
 
 $request = Request::capture();
 $response = new \Tcgv2\Bo\Responses\DashboardResponse();
